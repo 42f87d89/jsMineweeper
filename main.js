12bb454d0a624c32b9a81ba6f740b4b4a49caf2e
gustavo <gbuschle@hotmail.com>
2022-09-10 18:28:01 +0200

    asdf14

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ function createUI(cvs, ui, main) {
         if(!ui.audioCtx) {
             ui.audioCtx = new AudioContext();
             let temp = getAudioFile(ui.audioCtx, "pop.flac");
-            console.log(temp);
+            console.log(temp.value);
             ui.pop = temp.AudioBuffer;
         }
         ui.grid = makeGrid(+ui.w.value, +ui.h.value, +ui.size.value, 1, 3, 1, ui.p.value);
